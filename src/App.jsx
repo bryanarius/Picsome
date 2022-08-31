cac9e4bcc8504a2eada0b8db35df313ab5fdbc61
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-08-30 22:35:50 -0400

    React router dom, Context, New Components
    - Used React Router Dom to add multiple pages
    
    - Added Context Provider
    
    - Added and updated two components ( Header and Image)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {Routes, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
@@ -8,7 +9,13 @@ function App() {
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <Routes>
+                <Route exact path="/" element={<Photos />} />
+                
+                <Route path="/cart" element={<Cart />} />
+                  
+                
+            </Routes>
         </div>
     )
 }
