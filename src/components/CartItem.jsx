bddf60c87f972f40445f23eaee27db06860f6996
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-09-03 01:56:30 -0400

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,36 @@
+import React, {useState, useContext} from "react"
+import PropTypes from "prop-types"
+import {Context} from "../Context"
+// import useHover from "../hooks/useHover"
+
+
+function CartItem({item}) {
+
+    // const [hovered, ref] = useHover()
+    const [hovered, setHovered] = useState(false)
+    const {removeFromCart} = useContext(Context)
+
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+
     return (
+        <div className="cart-item">
+            <i 
+            className={iconClassName} onClick={() => removeFromCart(item.id)}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            >
+            </i>
+
+            <img src={item.url} width="130px" />
+            <p>$5.99</p>
+        </div>
+    )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape ({
+            url: PropTypes.string.isRequired
+        })
+}
+
+export default CartItem
